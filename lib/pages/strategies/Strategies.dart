569b937702c32e86b935bf26e1d6f8bdeae39c14
ivisonj <ivison_j@outlook.com>
2025-08-19 16:57:50 -0300

    feat: create bottomNavigationBar

---
+++ lib/pages/strategies/Strategies.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Strategies extends StatelessWidget {
+  const Strategies({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text('Strategies');
+  }
+}
